a4867502f992e141c99694904e7729a61821beb6
Kavya Goudar <ammuk2310@gmail.com>
2021-10-14 07:17:59 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'linkedinAuth': {
+      'clientID': '864fl6so9av29x', // your App ID
+      'clientSecret': '5aMZ1AxCwCzP37Ns', // your App Secret
+      'callbackURL': 'http://localhost:3000/auth/linkedin/callback'
+    }
+  }
